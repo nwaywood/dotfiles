3a3b3f5d9829045a34624433d92405ea9ec884dc
Nick Waywood <nwaywood@au1.ibm.com>
2015-11-25 16:07:27 +1100

    change tmux bar to have 12 hour clock

--- tmux/theme.sh
+++ tmux/theme.sh
@@ -48,8 +48,7 @@ set-option -g display-panes-colour $tm_color_inactive
 # clock
 set-window-option -g clock-mode-colour $tm_color_active
 
-
-tm_date="#[fg=$tm_color_inactive] %R %d %b"
+tm_date="#[fg=$tm_color_inactive] %l:%M%p %d %b"
 tm_host="#[fg=$tm_color_feature,bold]#h"
 tm_session_name="#[fg=$tm_color_feature,bold]$tm_icon #S"
 
