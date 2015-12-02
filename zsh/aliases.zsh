7c3154bd9a9a8641a9e59e2df484f6bcc4488a5e
Nick Waywood <nwaywood@au1.ibm.com>
2015-12-02 16:46:35 +1100

    added nvim alias and neovim specific settings

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,6 +3,9 @@
 # docker-machine
 alias dm='docker-machine'
 
+# neovim
+alias vim='nvim'
+
 # finder
 alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder'
 alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder'
