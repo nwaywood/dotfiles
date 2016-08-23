1b4111f41f22e3677f3a6f14166aa1327cdc2d63
Nick Waywood <nwaywood@au1.ibm.com>
2016-08-24 09:47:26 +1000

    added git aliases and clear alias to zsh config

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -7,7 +7,13 @@ alias dm='docker-machine'
 alias vim='nvim'
 alias mvim='nyaovim'
 
+# git
+alias gs='git status'
+alias g='git'
+
 # finder
 alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder'
 alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder'
 
+# misc
+alias c='clear'
