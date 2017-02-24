f1c3f3aac02d7e0ca5f859b8b6ec94153459cf88
Nick Waywood <nwaywood@au1.ibm.com>
2017-02-24 15:42:22 +1100

    Added fast light/dark theme switching with 'light' and 'dark' commands

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -15,5 +15,10 @@ alias g='git'
 alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder'
 alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder'
 
+# iterm theme
+# http://superuser.com/questions/414560/is-there-a-keyboard-shortcut-to-alternate-between-color-schemes-in-iterm2
+alias light='it2prof Light'
+alias dark='it2prof Default'
+
 # misc
 alias c='clear'
