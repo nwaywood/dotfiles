cb4e541bf3f34546545b967397d81f8932eaf189
Nick Waywood <nwaywood@au1.ibm.com>
2017-04-18 10:27:15 +1000

    Cleaned up install script

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+command_exists() {
+	type "$1" > /dev/null 2>&1
+
+}
+
+echo "Symlinking dotfiles"
+source install/link.sh
+
 if [ "$(uname)" == "Darwin" ]; then
 	echo -e "\n\nRunning on OSX"
 
@@ -16,16 +24,17 @@ git clone https://github.com/chriskempson/base16-shell.git $HOME/.dotfiles/.conf
 echo "Installing tpm (tmux package manager)"
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-echo "Symlinking dotfiles"
-source install/link.sh
 
-echo "go getting all the things"
-source install/go.sh
+# zsh setup
+if ! command_exists zsh; then
+    echo "zsh not found. Please install and then re-run installation scripts"
+    exit 1
+elif ! [[ $SHELL =~ .*zsh.* ]]; then
+    echo "Configuring zsh as default shell"
+    chsh -s $(which zsh)
+fi
 
 echo "installing oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-echo "Configuring zsh as default shell"
-chsh -s $(which zsh)
 
 echo "Done!"
