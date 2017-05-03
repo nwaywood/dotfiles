809b8addf4c9494a0aa7cf5ace6d25bf76edfa08
Nick Waywood <nwaywood@au1.ibm.com>
2017-05-03 17:00:30 +1000

    added bash script for vscode extensions

---
+++ install/vscode.sh
@@ -0,0 +1,21 @@
+# general 
+code --install-extension alefragnani.project-manager
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension shinnn.stylelint
+
+# theming
+code --install-extension azemoh.one-monokai
+code --install-extension file-icons.file-icons
+code --install-extension robertohuertasm.vscode-icons
+code --install-extension qinjia.seti-icons
+code --install-extension zhuangtongfa.Material-theme
+
+# languages support
+code --install-extension lukehoban.Go
+
+# misc
+code --install-extension flowtype.flow-for-vscode
+code --install-extension jpoissonnier.vscode-styled-components
+code --install-extension vscodevim.vim
+code --install-extension anseki.vscode-color
+code --install-extension prettier-vscode
