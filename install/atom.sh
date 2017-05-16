442363978f3d9d8723e275b5b60f174d956965fa
Nick Waywood <nwaywood@au1.ibm.com>
2017-05-16 13:33:05 +1000

    updated atom shell install script

--- install/atom.sh
+++ install/atom.sh
@@ -1,9 +1,17 @@
 # general
 apm install atom-beautify
+apm install atom-open-marked
 apm install file-icons
 apm install project-manager
 apm install platformio-ide-terminal
 apm install atom-terminal
+apm install goto-definition
+apm install todo-show
+apm install pain-split
+
+# git
+apm install git-blame
+apm install git-plus
 
 # frontend
 apm install pigments
@@ -25,20 +33,17 @@ apm install narrow
 apm install language-elm
 apm install language-haskell
 apm install language-viml
+apm install language-babel
 apm install atom-ternjs
+apm install prettier-atom
 
-apm install autocomplete-go
-apm install builder-go
-apm install go-config
-apm install go-get
 apm install go-plus
-apm install gofmt
-apm install gometalinter-linter
-apm install navigator-godef
-apm install tester-go
+apm install go-debug
+apm install go-signature-statusbar
 
 # linting
 apm install linter
+apm install linter-ui-default
 apm install linter-golinter
 apm install linter-hlint
 apm install linter-htmlhint
