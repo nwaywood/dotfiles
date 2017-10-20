057520b7a6ed18165db755a14ae3e78b2ca37fa8
Nick Waywood <nwaywood@au1.ibm.com>
2017-10-20 16:42:12 +1100

    Nicer osx dock behavior

--- install/osx.sh
+++ install/osx.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+# NOTE: Need to run `killall Dock` to refresh settings
+
 echo "Disable press-and-hold for keys in favor of key repeat"
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
@@ -9,7 +11,7 @@ defaults write NSGlobalDomain KeyRepeat -int 2 # lower = faster, 1 delta = 15ms,
 echo "Set a shorter Delay until key repeat"
 defaults write NSGlobalDomain InitialKeyRepeat -int 15 # lower = faster, 1 delta = 25ms, 15 = 225ms
 
-echo "Make the dock autoshow after 2 seconds"
+echo "Make the dock autoshow after half a second"
 # https://apple.stackexchange.com/questions/59556/is-there-a-way-to-completely-disable-dock
-defaults write com.apple.dock autohide-delay -float 2
-
+# float value is in seconds
+defaults write com.apple.dock autohide-delay -float 0.5
