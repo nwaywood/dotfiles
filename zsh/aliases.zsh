2bf988b2919f51d5cebacd03566bd1cbc87f340c
Nick Waywood <nwaywood@au1.ibm.com>
2019-10-29 10:15:07 +1100

    haskell and doom zsh stuff

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -28,6 +28,7 @@ alias vimf='vim "+Denite file_rec"'
 alias vimn='vim +NERDTreeToggle'
 alias tree='tree -I "node_modules"'
 alias find='find . -name'
+alias vimg='open -a emacs'
 
 mkcd () {
     test -d "$1" || mkdir "$1" && cd "$1"
