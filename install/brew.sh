c3b725356c3d481d9f88136e8758f40c81e4d126
Nick Waywood <nick.waywood@zendesk.com>
2021-03-05 15:50:15 +1100

    Fixed some things from settings up new laptop

--- install/brew.sh
+++ install/brew.sh
@@ -3,20 +3,12 @@
 echo "Installing homebrew packages..."
 
 # brew stuff
-brew tap caskroom/cask
+# brew tap caskroom/cask
 
 # core development tools
 brew install git
-# brew install macvim --with-override-system-vim
-brew install reattach-to-user-namespace
 brew install tmux
 brew install zsh
-
-# languages and langauge tools
-brew install go --with-cc-all
-# https://github.com/bitemyapp/learnhaskell
-brew install haskell-stack
-brew cask install elm-platform
 
 # CLI tools
 brew install z
@@ -30,15 +22,11 @@ brew install the_silver_searcher # good grep
 brew install ripgrep # good grep
 brew install --HEAD https://raw.githubusercontent.com/junegunn/blsd/master/blsd.rb
 
-# neovim and atom
-brew install neovim/neovim/neovim
+# neovim
+brew install neovim
 brew install python3
-brew cask install atom
-
-
-# for vim tagbar
-brew install ctags-exuberant
-brew install gotags
+brew install node # for coc.nvim
+brew install go # for hexokinase
 
 # gif creation app
 # used by t9md - https://www.cockos.com/licecap/
