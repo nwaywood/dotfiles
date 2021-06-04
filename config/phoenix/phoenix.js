bafc1754982be14d7538c71194425202bcb3a081
Nick Waywood <nick.waywood@zendesk.com>
2021-06-04 13:11:04 +1000

    Added phoenix config for launching/focusing apps

---
+++ config/phoenix/phoenix.js
@@ -0,0 +1,32 @@
+Phoenix.set({
+  daemon: true,
+  openAtLogin: true
+});
+
+const MOD1 = ['cmd', 'ctrl', 'alt'];
+const MOD2 = ['alt', 'shift'];
+
+
+Key.on('t', MOD2, function() {
+    App.launch("Kitty").focus();
+});
+
+Key.on('s', MOD2, function() {
+    App.launch("Slack").focus();
+});
+
+Key.on('c', MOD2, function(){
+  App.launch('Google Chrome').focus();
+});
+
+Key.on('f', MOD2, function(){
+  App.launch('Firefox').focus();
+});
+
+Key.on('e', MOD2, function(){
+  App.launch('Visual Studio Code').focus();
+});
+
+Key.on('z', MOD2, function(){
+  App.launch('zoom.us').focus();
+});
