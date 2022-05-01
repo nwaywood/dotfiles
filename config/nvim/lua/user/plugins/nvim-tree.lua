d43abb17e103f9b8ec3503e0d3433bbd38cae361
Nick Waywood <nick.waywood@zendesk.com>
2022-05-01 18:00:13 +1000

    more nvim-tree

--- config/nvim/lua/user/plugins/nvim-tree.lua
+++ config/nvim/lua/user/plugins/nvim-tree.lua
@@ -2,6 +2,7 @@ local keymap = vim.api.nvim_set_keymap -- local alias for set_keymap function
 local opts = { noremap = true, silent = true }
 
 keymap("n", "<leader>e", ":NvimTreeToggle<cr>", opts)
+keymap("n", "<leader>n", ":NvimTreeFindFile<cr>", opts)
 
 nvim_tree_config = require "nvim-tree.config"
 local tree_cb = nvim_tree_config.nvim_tree_callback
@@ -25,9 +26,7 @@ require'nvim-tree'.setup {
     },
 }
 
--- TODO: grey out gitignored files https://github.com/kyazdani42/nvim-tree.lua/issues/903
-
 vim.g.nvim_tree_group_empty = 1 -- group empty dirs on one line like vscode
-
+vim.g.nvim_tree_git_hl = 1 -- color folder/file name to match its git status
 -- close vim if nvim-tree is last buffer open
 vim.cmd("autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif")
