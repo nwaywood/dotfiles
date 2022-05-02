0cca697e6768184cf19f60e589b5e9f5f7288060
Nick Waywood <nick.waywood@zendesk.com>
2022-05-02 21:25:23 +1000

    nvim-lightbulb config

--- config/nvim/lua/user/plugins.lua
+++ config/nvim/lua/user/plugins.lua
@@ -80,6 +80,7 @@ return packer.startup(function(use)
     'ruifm/gitlinker.nvim',
     requires = 'nvim-lua/plenary.nvim',
   }
+  use "kosayoda/nvim-lightbulb" -- lightbulb for code actions
   
   -- Vim language enhancements
   use 'tpope/vim-unimpaired' -- mappings which are simply short normal mode aliases for commonly used ex commands
---
+++ config/nvim/lua/user/plugins/nvim-lightbulb.lua
@@ -0,0 +1,10 @@
+
+-- vim.cmd [[autocmd CursorHold,CursorHoldI * lua require'nvim-lightbulb'.update_lightbulb()]]
+local lightbulb_group = vim.api.nvim_create_augroup("lightbulb", { clear = true })
+vim.api.nvim_create_autocmd("CursorHold,CursorHoldI", {
+  pattern = "*",
+  callback = function()
+      require'nvim-lightbulb'.update_lightbulb()
+  end,
+  group = lightbulb_group,
+})
