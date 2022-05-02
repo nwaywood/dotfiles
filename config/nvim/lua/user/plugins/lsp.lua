54ecaba2c87e9c67cb493cfa80580cbaca6ba68a
Nick Waywood <nick.waywood@zendesk.com>
2022-05-02 21:13:51 +1000

    more lsp bindings

--- config/nvim/lua/user/plugins/lsp.lua
+++ config/nvim/lua/user/plugins/lsp.lua
@@ -53,13 +53,13 @@ vim.diagnostic.config(config)
 
 -- Mappings.
 
--- See `:help vim.diagnostic.*` for documentation on any of the below functions
+-- See `:help vim.diagnostic.*` for documentation on any of the "vim.diagnostic" functions
+-- See `:help vim.lsp.*` for documentation on any of the "vim.lsp" functions
 local opts = { noremap=true, silent=true }
 local keymap = vim.api.nvim_set_keymap -- local alias for set_keymap function
 keymap('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
 keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
 keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
-keymap('n', '<leader>d', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
 keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
 keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
 keymap('n', 'gh', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
@@ -67,21 +67,22 @@ keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
 keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
 keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
 
+local wk = require("which-key")
+wk.register({
+	d = { "<cmd>lua vim.diagnostic.setloclist()<CR>", "Diagnostics" },
+  l = {
+	  name = "LSP",
+	  f = { "<cmd>lua vim.lsp.buf.formatting()<CR>", "Format" },
+	  a = { "<cmd>lua vim.lsp.buf.code_action()<CR>", "Actions" },
+	  d = { "<cmd>lua vim.diagnostic.setloclist()<CR>", "Diagnostics" },
+  },
+}, { prefix = "<leader>"})
+
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
   -- Enable completion triggered by <c-x><c-o>
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-  -- NOTE: I have moved the bindings I'm actually using atm out of on_attach so they are available for metals as well
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
---   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
---   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
---   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gh', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
---   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
---   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
---   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
 
 -- Not set up
 --   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
@@ -89,8 +90,6 @@ local on_attach = function(client, bufnr)
 --   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
 --   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
 --   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
---   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
---   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 end
 
 -- Add additional capabilities supported by nvim-cmp
