9866c5c4af0f7323f8b237ba429da3473a08d736
Nick Waywood <nick.waywood@zendesk.com>
2022-05-09 16:54:15 +1000

    alpha binding

--- config/nvim/lua/user/plugins/alpha.lua
+++ config/nvim/lua/user/plugins/alpha.lua
@@ -7,10 +7,11 @@ local dashboard = require("alpha.themes.dashboard")
 
 dashboard.section.buttons.val = {
 	dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
-	dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+	dashboard.button("e", "  Open Explorer", ":NvimTreeToggle<CR>"),
 	dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
 	dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
 	dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
+	dashboard.button("n", "  New file", ":ene <BAR> startinsert <CR>"),
 	dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua <CR>"),
 	dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
 }
