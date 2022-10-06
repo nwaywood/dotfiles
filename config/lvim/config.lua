6d015ed953abdb5ceadaead07063b2b370bce718
Nick Waywood <nick.waywood@zendesk.com>
2022-10-06 17:57:16 +1100

    disable auto installation of lsp's

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -132,10 +132,11 @@ lvim.builtin.treesitter.highlight.enable = true
 lvim.lsp.installer.setup.ensure_installed = {
 	"sumeko_lua",
 	"jsonls",
+	"yamlls",
 }
 
 -- ---@usage disable automatic installation of servers
--- lvim.lsp.installer.setup.automatic_installation = false
+lvim.lsp.installer.setup.automatic_installation = false
 
 -- ---configure a server manually. !!Requires `:LvimCacheReset` to take effect!!
 -- ---see the full default list `:lua print(vim.inspect(lvim.lsp.automatic_configuration.skipped_servers))`
