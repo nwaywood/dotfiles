369b2385f4b41e1e7ed9cf498e1ba17814d9d3de
Nick Waywood <nick.waywood@zendesk.com>
2023-03-23 16:00:33 +1100

    updated to lunarvim 1.2

--- config/lvim/lua/user/lsp.lua
+++ config/lvim/lua/user/lsp.lua
@@ -8,7 +8,6 @@ lvim.builtin.which_key.mappings["lD"] = { "<cmd>Trouble workspace_diagnostics<cr
 
 -- make sure server will always be installed even if the server is in skipped_servers list
 lvim.lsp.installer.setup.ensure_installed = {
-	"sumeko_lua",
 	"jsonls",
 	"yamlls",
 }
