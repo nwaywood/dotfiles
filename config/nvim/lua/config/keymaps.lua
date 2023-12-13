e046e53f243657cf126f7c1cd3991a5a2ccec10b
Nick Waywood <nick.waywood@zendesk.com>
2023-12-13 15:10:50 +1100

    Add jk binding to lazyvim

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local opts = { noremap = true, silent = true }
+local keymap = vim.keymap.set
+
+keymap("i", "jk", "<ESC>", opts)
