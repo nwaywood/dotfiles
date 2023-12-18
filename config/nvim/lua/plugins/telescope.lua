846ce2c1b6c0198619431fbe34156edc48f9fdbf
Nick Waywood <nick.waywood@zendesk.com>
2023-12-18 17:04:40 +1100

    custom telescope config for lazyvim

---
+++ config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,52 @@
+local Util = require("lazyvim.util")
+local actions = require "telescope.actions"
+  -- change some telescope options and a keymap to browse plugin files
+return {
+    "nvim-telescope/telescope.nvim",
+    keys = {
+      { "<leader>a", Util.telescope("live_grep"), desc = "Grep project" },
+    },
+    opts = {
+      defaults = {
+        layout_strategy = "horizontal",
+        layout_config = { prompt_position = "top" },
+        sorting_strategy = "ascending",
+        path_display = { "smart" },
+        mappings = {
+          i = {
+            ["<C-n>"] = actions.cycle_history_next,
+            ["<C-p>"] = actions.cycle_history_prev,
+
+            ["<C-j>"] = actions.move_selection_next,
+            ["<C-k>"] = actions.move_selection_previous,
+
+            ["<C-g>"] = actions.close, -- default is C-c
+            ["<C-s>"] = actions.select_horizontal, -- default is C-x
+
+            ["<C-u>"] = actions.preview_scrolling_up,
+            ["<C-d>"] = actions.preview_scrolling_down,
+
+            ["<C-b>"] = actions.results_scrolling_up,
+            ["<C-f>"] = actions.results_scrolling_down,
+            
+            -- ["<c-t>"] = trouble.open_with_trouble,
+          },
+
+          n = {
+            ["<C-g>"] = actions.close, -- default is C-c
+            ["<C-s>"] = actions.select_horizontal, -- default is C-x
+
+            ["<C-u>"] = actions.preview_scrolling_up,
+            ["<C-d>"] = actions.preview_scrolling_down,
+
+            ["<C-b>"] = actions.results_scrolling_up,
+            ["<C-f>"] = actions.results_scrolling_down,
+
+            ["?"] = actions.which_key,
+            
+            -- ["<c-t>"] = trouble.open_with_trouble,
+          },
+        },
+      },
+    },
+}
