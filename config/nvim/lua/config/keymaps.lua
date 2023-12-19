7be44fd8c4f5cbd9816be8ecf8bf6606ab90bf5e
Nick Waywood <nick.waywood@zendesk.com>
2023-12-19 17:38:25 +1100

    join lines command

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -8,6 +8,8 @@ local keymap = vim.keymap.set
 keymap("i", "jk", "<ESC>", opts)
 keymap("n", "\\", "\"_", opts) -- blackhole register shortcut
 
+keymap("n", "<space>j", "J", { desc = "Join lines", noremap = true})
+
 local lazyterm = function() Util.terminal(nil, { cwd = Util.root() }) end
 keymap("n", "<c-`>", lazyterm, { desc = "Terminal (root dir)" })
 keymap("t", "<C-`>", "<cmd>close<cr>", { desc = "Hide Terminal" })
