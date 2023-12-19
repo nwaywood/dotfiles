ec2cd46f23a3c1385eb15046a4e5b93a7a9cf180
Nick Waywood <nick.waywood@zendesk.com>
2023-12-19 17:10:39 +1100

    more lazyvim goodies
    neo-tree bindings and misc

--- config/nvim/lua/plugins/neo-tree.lua
+++ config/nvim/lua/plugins/neo-tree.lua
@@ -1,7 +1,28 @@
+local Util = require("lazyvim.util")
 return {
     "nvim-neo-tree/neo-tree.nvim",
     keys = {
         {"<leader>fe", false},
         {"<leader>fE", false},
+        { "<leader>e", function()
+          require("neo-tree.command").execute({ toggle = true, dir = Util.root() })
+        end,
+         desc = "Explorer NeoTree (root dir)", remap = true },
+        { "<leader>E", function()
+          require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
+        end,
+        desc = "Explorer NeoTree (cwd)", remap = true },
+    },
+    opts = {
+        window = {
+            mappings = {
+                ["l"] = "open",
+                ["o"] = "open",
+                ["h"] = "close_node",
+                ["s"] = "open_split",
+                ["v"] = "open_vsplit",
+                ["go"] = { "toggle_preview", config = { use_float = true, use_image_nvim = true } },
+            }
+        }
     }
 }
---
+++ config/nvim/lua/plugins/project.lua
@@ -0,0 +1,7 @@
+return {
+    "ahmedkhalf/project.nvim",
+    keys = {
+        {"<leader>fp", false},
+        { "<leader>p", "<Cmd>Telescope projects<CR>", desc = "Projects" },
+    }
+}
